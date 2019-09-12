fd9b982be59e9ed25edd13dc422d53567468c793
suzusime <tamagawajousuiro+git@gmail.com>
2019-09-12 21:26:13 +0900

    ビルドスクリプトを追加

---
+++ README.md
@@ -0,0 +1,13 @@
+# gcc-jman
+
+GCC(1)のmanpageを和訳する。
+
+## ビルド方法
+```sh
+$ cd gcc
+$ ./make.sh
+$ man doc/gcc.1
+```
+
+## メモ
+- manpageの生成元は invoke.texi。これはGCCのマニュアルの第三章にあたる
---
+++ gcc/make.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+(echo "@set version-GCC 9.2.0"; \
+ if [ "" = "experimental" ]; \
+ then echo "@set DEVELOPMENT"; \
+ else echo "@clear DEVELOPMENT"; \
+ fi) > gcc-vers.texiT
+echo @set srcdir `pwd` >> gcc-vers.texiT
+if [ -n "(GCC) " ]; then \
+  echo "@set VERSION_PACKAGE (GCC) " >> gcc-vers.texiT; \
+fi
+echo "@set BUGURL @uref{https://gcc.gnu.org/bugs/}" >> gcc-vers.texiT; \
+mv -f gcc-vers.texiT gcc-vers.texi
+echo timestamp > gcc.pod
+perl ./../contrib/texi2pod.pl ./doc/invoke.texi > gcc.pod
+echo timestamp > doc/gcc.1
+(pod2man --center="GNU" --release="gcc-9.2.0" --date=2019-08-12 --section=1 gcc.pod > doc/gcc.1.T$$ && \
+        mv -f doc/gcc.1.T$$ doc/gcc.1) || \
+        (rm -f doc/gcc.1.T$$ && exit 1)
+rm gcc.pod
+
